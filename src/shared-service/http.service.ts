38c867e6902d102573dd63e4f0c5cb4b62f02d80
Jose Hernandez <puch007@mail.fresnostate.edu>
2021-05-04 20:17:40 -0700

    project

--- src/shared-service/http.service.ts
+++ src/shared-service/http.service.ts
@@ -1,14 +1,42 @@
 import { Injectable } from '@angular/core';
-import { Headers } from '@angular/http';
-
+import { Http, Headers } from '@angular/http';
+import {environment} from '../environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
-
+apiURL = '';
   constructor(
-
+private http: Http
   ) {
+    this.apiURL = environment.apiURL;
+  }
+
+  testing() {
+    console.log('fromtesting/');
+  }
+  async get(path: string) {
+    const resp = await this.http.get(this.apiURL + path, this.headers).toPromise();
+    console.log('response', resp.json());
+    return resp.json();
+
+
+  }
+  async post(path: string , payload: any) {
+    const resp = await this.http.post(this.apiURL + path, payload, this.headers).toPromise();
+    console.log('from post', resp.json());
+    return resp.json();
+  }
+  async Put(path: string, payload: any) {
+const resp = await this.http.put(this.apiURL + path, payload, this.headers).toPromise();
+console.log('serviceput', resp.json());
+return resp.json();
+
+  }
+  async delete(path: string ) {
+    const resp = await this.http.delete(this.apiURL + path, this.headers).toPromise();
+    return resp.json();
+
   }
 
   get headers() {
@@ -22,4 +50,5 @@ export class HttpService {
       withCredentials: true
     };
   }
+
 }
